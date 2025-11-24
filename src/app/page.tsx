6ef67a30682bc0fee00cc7d5f2aad540cb6fdae1
Toprak <birbentoprak@gmail.com>
2025-11-24 15:02:21 +0100

    Modified the website's UI and some functionality.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,28 +4,35 @@ import styles from './Home.module.css';
 import React, {useState } from 'react'
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
-import GridLayout from "react-grid-layout";
 import { motion } from "motion/react"
 import { AnimatePresence } from "motion/react"
 import Image from "next/image"
+import { Row, Col } from 'antd';
+import { Card } from 'antd';
+import { Layout } from 'antd';
+import { Button } from 'antd';
+import {LinkedinOutlined, GithubOutlined } from '@ant-design/icons';
+
+
+
+
 
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.main_content}> 
-        {NavBar()}
-        {Torpak()}
-        {About()}
-      </div>
-        {Transition()}
-      <div className={styles.main_content}>
-        {Project()}      
-      </div>
-      <div className={styles.end_content}> 
-        {Contact()}
-        {PageFooter()}
-      </div>
+
+  return(
+    <div>
+      <Layout>
+        <Layout.Header >{NavBar()}</Layout.Header>
+        <Layout>
+          <Layout.Content >{Torpak()}</Layout.Content>
+          <Layout.Content >{About()}</Layout.Content>
+          <Layout.Content >{Transition()}</Layout.Content>
+          <Layout.Content >{Project()}</Layout.Content>          
+          <Layout.Content >{Contact()}</Layout.Content>
+          <Layout.Content >{PageFooter()}</Layout.Content>
+        </Layout>
+      </Layout>
     </div>
   );
 }
@@ -44,9 +51,9 @@ function NavBar() {
     <nav className={styles.navbar}>
       <ul>
           <li><a href="#toprak"><strong>toprak</strong></a></li>
-          <li><a href="#about">about me</a></li>
+          <li><a href="#resume">contact me</a></li>
           <li><a href="#project">projects</a></li>
-          <li><a href="#contact">contact me</a></li>
+          <li><a href="#about">about me</a></li>
       </ul>
     </nav>
   );
@@ -64,73 +71,60 @@ function Torpak() {
             </clipPath>
           </defs>          
         </svg>
-        <Image src="/torpak.png" alt="torpak" className={styles.image}/>
+        <Image src="/torpak.png" alt="torpak" width={907} height={1384}className={styles.image}/>
         
       </div>
-      <h1 className={styles.grid_item}><strong>2025</strong> graduate of Vrije Universiteit <strong>BSc Computer Science</strong></h1>
+      <h1 className={styles.grid_item}> Graduate of <strong> BSc Computer Science</strong></h1>
       <p className={styles.grid_item} >Here you can check out what I&apos;m working on!</p>
     </div>
   );
 }
 
 function About() {
-  return (
-      <div id="about" className={styles.grid_sectors}>
-        <SectionTitle text_info="Intro"/>
-        <Divider/>
-        <div className={styles.grid_item}>
-          <p> 
-            I was born and raised in Istanbul, and I am now located in Amsterdam. My academic journey builds on a foundation of curiosity and drive, 
-            reflected in my diverse projects and experiences such as development of an application or making a portfolio website from scratch or even 
-            my contribution in robotics field. Before my time in Amsterdam, I practiced my leadership and technical skills through four years of involvement 
-            in FIRST Robotics, where I was introduced to many concepts such as teamwork, problem solving and during my last year I was honored to lead 
-            my team in designing and building competition robots. These experiences taught me the value of teamwork, resilience, and pursuing innovation with a clear purpose.
-          </p> 
-        </div>
-      </div>
+
+  return(
+    <div id = "about" style={{paddingLeft: '122px', paddingRight: '122px', paddingBottom: '50px'}}> 
+      <Layout>
+        <Layout> 
+          <Layout.Sider style={{ fontSize: '36px', fontWeight: 'bold', color:'#E43D12'}}> <SectionTitle text_info='About'/> </Layout.Sider>
+          <Layout.Content> 
+            <p style = {{ fontSize: '20px', textAlign: 'justify', lineHeight: '1.6'}}> 
+              I was born and raised in Istanbul, and I am now located in Amsterdam. My academic journey builds on a foundation of curiosity and drive, 
+              reflected in my diverse projects and experiences such as development of an application or making a portfolio website from scratch or even 
+              my contribution in robotics field. Before my time in Amsterdam, I practiced my leadership and technical skills through four years of involvement 
+              in FIRST Robotics, where I was introduced to many concepts such as teamwork, problem solving and during my last year I was honored to lead 
+              my team in designing and building competition robots. These experiences taught me the value of teamwork, resilience, and pursuing innovation with a clear purpose.
+            </p>
+          </Layout.Content>
+        </Layout>
+      </Layout>
+    </div>
+    
   );
 }
 
 function Project() {
-  const layout = [
-    { i: "a", x: 0, y: 0, w: 5, h: 12, static: true },
-    { i: "b", x: 5, y: 0, w: 4, h: 11, static: true},
-  ];
-  return (
-    <div className={styles.project_sector}>
-      <div id="project" className={styles.grid_sectors}>
-        <SectionTitle text_info="Projects"/>  
-        <Divider/>
-        <div className={styles.grid_item}>
-          <GridLayout
-            className="layout"
-            layout={layout}    
-            cols={12}
-            rowHeight={30}
-            width={1200}
-          >
-              <div key="a" className={styles.proj_item}> 
-                <a href="https://github.com/toprakbirben/GoogleCalendar-Automation">
-                  <GridItem 
-                    text_header = 'history/calendar automation' 
-                    idea='What if there was a way to see how many hours I spent on a webpage and organise my schedule more efficiently; just like screen time report'
-                    desc='This is a python script to increase productivity by integrating your history to your calendar via Google Calendar API.'
-                    github='https://github.com/toprakbirben/GoogleCalendar-Automation'/>
-                </a>
-              </div>
-            <div key="b"className={styles.proj_item}> 
-              <a href="https://github.com/toprakbirben/portfolio">
-                <GridItem
-                  text_header = 'this.website;'
-                  idea= 'Instead of filling out templates, I decided to build my own website with my own design.'
-                  desc='This website is built developed on Next.js and React, and designed in Figma.'
-                  github='https://github.com/toprakbirben/portfolio'/> 
-              </a>
-            </div>
-          </GridLayout>
-        </div>
-      </div>
-    </div>
+
+  const portfolioText = "A clean, responsive personal website built from scratch to showcase my work, experience, and technical skills. It's designed to serve as a digital resume and portfolio hub, featuring project highlights, links to my GitHub repos, and a simple yet modern UI. This project demonstrates my web design abilities, proficiency with HTML/CSS/JS, and commitment to maintain a professional online presence.";
+  const moreThanTasksText = "A productivity tool focused on task management with an emphasis on meaning and self-reflection. MoreThanTasks lets users not only track tasks, but also categorize them by purpose, set long‑term visions, and reflect on progress. It's more than a to-do list — it helps structure work around personal goals. I built this to improve my own productivity workflow and showcase my skills in building full-stack, goal-oriented applications.";
+  const calendarAutomationText = "A Python based tool that automates the creation of Google Calendar events by analyzing your Chrome browsing history. Using SQL queries on the Chrome history database, it extracts key information such as how long you spent on a tab and when you last visited it—and then automatically generates corresponding events in your Google Calendar via the Calendar API. This project showcases my skills in data processing, API integration, and automation.";
+
+  return(
+    <Layout style={{paddingLeft: '122px', paddingRight: '122px', paddingTop: '50px', paddingBottom: '100px'}} id="project">
+      <Layout> 
+        <Layout.Sider style={{ fontSize: '36px', fontWeight: 'bold', color:'#E43D12'}}> <SectionTitle text_info='Projects'/> </Layout.Sider>
+        <Layout.Content> 
+          <Row gutter={[16, 16]}>
+            <Col span={12}> <Card title="Personal Portfolio Website" style={{ backgroundColor: '#E7E4DA', borderColor: '#E43D12', borderWidth: 1.5, borderRadius: 12 }} extra={<a href="https://github.com/toprakbirben/portfolio">More</a>}>{portfolioText}</Card> </Col> 
+            <Col span={12}> <Card title="more-than-tasks" style={{ backgroundColor: '#E7E4DA', borderColor: '#E43D12', borderWidth: 1.5, borderRadius: 12 }} extra={<a href="https://github.com/toprakbirben/morethantasks">More</a>}>{moreThanTasksText} </Card> </Col> 
+
+            <Col span={12}> <Card title="Calendar Automation" style={{ backgroundColor: '#E7E4DA', borderColor: '#E43D12', borderWidth: 1.5, borderRadius: 12 }} extra={<a href="https://github.com/toprakbirben/GoogleCalendar-Automation">More</a>}>{calendarAutomationText}</Card> </Col> 
+          </Row>
+        </Layout.Content>
+      </Layout>
+    </Layout>
+
+    
   );
 }
 
@@ -141,13 +135,20 @@ function Contact() {
     setOpenAccordion(openAccordion === accordion ? null : accordion);
   };
 
-  return (
-    <div className={styles.contact} id="contact"> 
-      <div className={styles.contact_container}> 
-        <div className={styles.grid_sectors}> 
-          <SectionTitle text_info="Resume"/>  
-          <Divider/>
-          <div className={styles.grid_item}> 
+  const openInNewTab = (filePath: string) => {
+    window.open(filePath, "_blank", "noopener,noreferrer");
+  };
+
+  return(
+    <div id = "resume" style={{paddingLeft: '122px', paddingRight: '122px', paddingBottom: '100px'}}> 
+      <Layout>
+        <Layout> 
+          <Layout.Sider style={{ fontSize: '36px', fontWeight: 'bold', color:'#E43D12'}}>
+            <a onClick={() => openInNewTab("/SarpToprakBirben_Resume.pdf")}> 
+              <SectionTitle text_info='Resume'/>
+            </a> 
+          </Layout.Sider>
+          <Layout.Content> 
             <motion.div className={styles.parent_accordion}>
               <AnimatePresence>
                 <motion.div
@@ -172,10 +173,16 @@ function Contact() {
                     className={styles.content}
                   >
                     <div className={styles.group}> 
-                      <h1> Vrije Universiteit Amsterdam - <i>Class of 2025</i></h1>
-                      <p> Bachelor&apos;s in Computer Science</p>
+                      <h1 className={styles.edu_line}>
+                        <span>
+                          <strong>Vrije Universiteit</strong> - Bsc Computer Science
+                        </span>
+
+                        <span className={styles.edu_year}>
+                          <i>2025</i>
+                        </span>
+                      </h1>                  
                     </div>
-                    <h1> Hisar School - <i> Class of 2022</i></h1>
                   </motion.div>
                 )}
               </AnimatePresence>
@@ -204,71 +211,54 @@ function Contact() {
                     exit={{ opacity: 0 }}
                     className={styles.content}
                   >
-                    <div> 
-                      <h1>Robotics</h1>
-                      <ul> 
-                        <li>Took responsibility of team&apos;s leadership and mentorship during the 19/20 & 21/22 season.</li>
-                        <li>Delved into various sub-sections such as electronics, programming, manufacturing and designing of the robot</li>
-                        <li>Nominated in various awards and won Industrial Safety in 2019</li>
-                      </ul>
+                    <div className={styles.group}> 
+                      <div className={styles.edu_item}>
+                        <h1 className={styles.edu_line}>
+                          <span>
+                            <strong>Robotics</strong> - FIRST Robotics Competition
+                          </span>
+
+                          <span className={styles.edu_year}>
+                            <i>2018-2022</i>
+                          </span>
+                        </h1>
+
+                        <ul className={styles.edu_list}>
+                          <li>Took responsibility of team&apos;s leadership and mentorship during the 19/20 & 21/22 season.</li> 
+                          <li>Delved into various sub-sections such as electronics, programming, manufacturing and designing of the robot</li> 
+                          <li>Nominated in various awards and won Industrial Safety in 2019</li>
+                        </ul>
+                      </div>
+
+                      <div className={styles.edu_item}>
+                        <h1 className={styles.edu_line}>
+                          <span>
+                            <strong>Coding Summit</strong> - Hisar School
+                          </span>
+
+                          <span className={styles.edu_year}>
+                            <i>2021 & 2022</i>
+                          </span>
+                        </h1>
+
+                        <ul className={styles.edu_list}>
+                          <li>Volunteered to teach introductory Java programming to all age groups with diverse academic backgrounds.</li> 
+                          <li>Explained core concepts such as classes, methods and object-oriented programming in an accessible and engaging way.</li> 
+                          <li>Supported learners by answering questions, debugging code, and reinforcing problem-solving skills.</li> 
+                          <li>Students experienced hands-on exercises and small projects, to build their first functional Java code.</li>
+                        </ul>
+                      </div>
                     </div>
+
+
                   </motion.div>
                 )}
               </AnimatePresence>
             </motion.div>
-
-            <motion.div className={styles.parent_accordion}>
-              <AnimatePresence>
-                <motion.div
-                  key="skills"
-                  onClick={() => toggleAccordion('skills')}
-                >
-                  <motion.div className={styles.title}>
-                    Skills
-                  </motion.div>
-                </motion.div>
-                {openAccordion === 'skills' && (
-                  <motion.div
-                    key="skills-content"
-                    initial={{ opacity: 0 }}
-                    animate={{
-                      opacity: 1,
-                      transition: {
-                        duration: 0.3,
-                      },
-                    }}
-                    exit={{ opacity: 0 }}
-                    className={styles.content}
-                  >
-                    <div> 
-                      <p>Proficient in Java, Python, Figma, HTML, CSS, React & its Frameworks, Problem Solving & App Development.</p>
-                    </div>
-                  </motion.div>
-                )}
-              </AnimatePresence>
-            </motion.div>
-
-          </div>
-        </div>
-      </div>
+          </Layout.Content>
+        </Layout>
+      </Layout>
     </div>
-  );
-}
-
-function Divider() {
-  return (
-    <Image  src="/divider.svg" alt="divider" className={`${styles.grid_item} ${styles.divider}`} />
-  );
-}
-
-function GridItem({text_header, idea, desc, github}: {text_header: string, idea: string, desc:string, github: string}) {
-  return (
-    <div className={styles.proj_box}> 
-      <p className ={styles.proj_head}>{text_header}</p>
-      <p><br/>{idea}</p>
-      <p><br/>{desc}</p>
-      <footer> <a href={github}> <br/> <i>for more information</i> </a> </footer>
-    </div> 
   );
 }
 
@@ -280,22 +270,27 @@ function SectionTitle({text_info}: {text_info: string}) {
   );
 }
 
+//Direkt contact me
 function PageFooter() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.footer_container}>
-
-        <div className={styles.footer_item} >
-          <a href="https://linkedin.com/in/toprakbirben"> 
-          <Image src="/linkedin.webp" alt="linkedIn image"/>   
-            <p>toprakbirben</p>
-          </a>
+      <div>
+        <h1>Contact me</h1>
+        <div>
+          <Button type="link" icon={<LinkedinOutlined />} size="large" style={{color: 'inherit', textDecoration: 'none' }}>
+            <a href="https://www.linkedin.com/in/toprakbirben/">
+              LinkedIn
+            </a>
+          </Button>
+          <Button type="link" icon={<GithubOutlined />} size="large" style={{color: 'inherit', textDecoration: 'none' }}>
+            <a href="https://github.com/toprakbirben">
+              Github
+            </a>
+          </Button>
         </div>
-        <div className={styles.footer_item}>
-          <a href="https://github.com/toprakbirben"> 
-            <Image  src="/github.png" alt="github image"/>
-            <p>toprakbirben</p>
-          </a>
+        <div>
+          <p><strong>Email:</strong> birbentoprak@gmail.com</p>
+          <p><strong>Location:</strong> Amsterdam, Noord Holland</p>
         </div>
       </div>
     </footer>
